aaf37ac12412fff76d60e3223fad1ec3ce1043f5
Dr4x-k <jabe.4124@gmail.com>
2023-09-26 21:54:33 -0700

    Creación del modelo de usuario

---
+++ app/models/index.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const mysql = require('mysql2');
+
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize(
+  process.env.DB_DATABASE,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'mysql'
+  }
+);
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.users = require('./users.model')(sequelize, Sequelize);
+
+module.exports = db;
