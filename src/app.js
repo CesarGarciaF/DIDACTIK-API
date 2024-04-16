b0c36159513eeece6281be861c827eb29ac8ba9e
Dr4x-k <jabe.4124@gmail.com>
2024-04-16 11:22:02 -0600

    Correccion de origins

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(coockieParser());
 
 const allowedOrigins = [
-  "http://localhost:5173",
+  // "http://localhost:5173",
   "https://didacti-k.onrender.com",
 ];
 
